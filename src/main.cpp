21a92d713fade89e29d2ebe88826a4f672207420
zhouxu <zhouxu@simpretech.com>
2021-08-25 14:54:40 +0800

    1.for assignment01 work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,147 @@
-#include<cmath>
+#include "Triangle.h"
+#include "rasterizer.h"
 #include <Eigen/Eigen>
-#include<iostream>
+//#include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main(){
+constexpr double MY_PI = 3.1415926;
+using namespace std;
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
-    // Basic Example of cpp
-    std::cout << "Example of cpp \n";
-    float a = 1.0, b = 2.0;
-    std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
-    std::cout << std::sqrt(b) << std::endl;
-    std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
 
-    // Example of vector
-    std::cout << "Example of vector \n";
-    // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
-    // vector output
-    std::cout << "Example of output \n";
-    std::cout << v << std::endl;
-    // vector add
-    std::cout << "Example of add \n";
-    std::cout << v + w << std::endl;
-    // vector scalar multiply
-    std::cout << "Example of scalar multiply \n";
-    std::cout << v * 3.0f << std::endl;
-    std::cout << 2.0f * v << std::endl;
+    view = translate * view;
 
-    // Example of matrix
-    std::cout << "Example of matrix \n";
-    // matrix definition
-    Eigen::Matrix3f i,j;
-    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
-    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
-    // matrix output
-    std::cout << "Example of output \n";
-    std::cout << i << std::endl;
-    // matrix add i + j
-    // matrix scalar multiply i * 2.0
-    // matrix multiply i * j
-    // matrix multiply vector i * v
+    return view;
+}
 
-    using namespace std;
-    Eigen::Vector3f P(2.0, 1.0, 1);
-    Eigen::Matrix3f rotate_mat;
-    rotate_mat << std::cos(45.0/180.0 * acos(-1)), -sin(45.0 / 180.0 * acos(-1)), 0, 
-    sin(45.0/180.0 * acos(-1)), cos(45.0/180.0*acos(-1)),
-    0, 0, 0, 1;
-    Eigen::Matrix3f translate_mat;
-    double tx = 1.0;
-    double ty = 2.0;
-    cout << "旋转矩阵" << endl;
-    cout << rotate_mat << endl;
+Eigen::Matrix4f get_model_matrix(float rotation_angle)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
-    translate_mat << 1, 0, tx,
-                     0, 1, ty,
-                     0, 0, 1;
-    cout << "平移矩阵" << endl;
-    cout << translate_mat << endl;
-    Eigen::Vector3f result_after_rotate(0, 0, 0);
-    result_after_rotate = rotate_mat * P;
+    // TODO: Implement this function
+    // Create the model matrix for rotating the triangle around the Z axis.
+    // Then return it.
+    double radian;
+    radian = rotation_angle / 180 * MY_PI;
+    model << cos(radian), -sin(radian), 0, 0,
+             sin(radian), cos(radian), 0, 0, 
+             0, 0, 1, 0,
+             0, 0, 0, 1;
+    return model;
+}
 
-    std::cout <<"旋转变换的结果"<< endl;
-    std::cout << result_after_rotate << endl;
-    Eigen::Vector3f result_after_translate;
-    result_after_translate = translate_mat * result_after_rotate;
-    cout << "平移变换结果" << endl;
-    cout << result_after_translate << endl;
-    cin.get();
-    cin.get();
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+                                      float zNear, float zFar)
+{
+    // Students will implement this function
+
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+    // 首先，先求一个挤压矩阵
+    Eigen::Matrix4f prep2orth = Eigen::Matrix4f::Identity();
+    prep2orth << zNear, 0, 0, 0,
+                 0, zNear, 0, 0,
+                 0, 0, zNear + zFar, zNear - zFar,
+                 0, 0, 1, 0;
+    // 然后开始正交投影
+    double half_eye_radian = eye_fov * MY_PI / 2 / 180;
+    double t = -zNear * tan(half_eye_radian);
+    double b = -t;
+    //aspect_ratio 是屏幕的长宽比，所以有：
+    double r = t * aspect_ratio;
+    double l = -r;
+    //综上根据教程，得到正交变换矩阵，分为旋转和平移
+    Eigen::Matrix4f ortho_rotate = Eigen::Matrix4f::Identity();
+    ortho_rotate << 2 / (r - l), 0, 0, 0,
+                    0, 2 / (t - b), 0, 0, 
+                    0, 0, 2 / (zNear - zFar), 0,
+                    0, 0, 0, 1;
+    Eigen::Matrix4f ortho_trans = Eigen::Matrix4f::Identity();
+    ortho_trans << 1, 0, 0, -(r + l) / 2,
+                   0, 1, 0, -(t + b) / 2,
+                   0, 0, 1, -(zNear + zFar) / 2, 
+                   0, 0 ,0, 1;
+    // TODO: Implement this function
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+    Eigen::Matrix4f result_m_ortho = ortho_rotate * ortho_trans;
+    projection = result_m_ortho * prep2orth;
+    return projection;
+}
+
+int main(int argc, const char** argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc >= 3) {
+        command_line = true;
+        angle = std::stof(argv[2]); // -r by default
+        if (argc == 4) {
+            filename = std::string(argv[3]);
+        }
+        else
+            return 0;
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Eigen::Vector3f eye_pos = {0, 0, 5};
+
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+
+        if (key == 'a') {
+            angle += 10;
+        }
+        else if (key == 'd') {
+            angle -= 10;
+        }
+    }
+
     return 0;
 }
